566ec15848d277588b46c51be9c59ac89b4acdff
zhangqizong <zqz@szltech.com>
2023-08-04 10:57:36 +0800

    【手撸Spring源码】IOC容器

--- src/main/java/com/austin/springcode/beans/factory/config/AutowireCapableBeanFactory.java
+++ src/main/java/com/austin/springcode/beans/factory/config/AutowireCapableBeanFactory.java
@@ -12,6 +12,7 @@ import com.austin.springcode.beans.factory.BeanFactory;
  */
 public interface AutowireCapableBeanFactory extends BeanFactory {
 
+
     /**
      * 执行 BeanPostProcessors 接口实现类的 postProcessBeforeInitialization 方法
      *
